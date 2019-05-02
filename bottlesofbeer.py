0a72a915ad4c26200572c20df5d759b7d88759fd
EmilG <10610649+emilgab@users.noreply.github.com>
2019-05-02 20:32:38 +0200

    Removed the list-object from the script
    Instead we print out the numbers and lyrics directly from the range-function instead of storing values in a list first.

--- bottlesofbeer.py
+++ bottlesofbeer.py
@@ -7,29 +7,30 @@
 # uses the time module to pause before each line output
 import time
 
-# list to store our values in
-bottles = []
-
-# iterates through the range function and appends each number
+# iterates through the range function and prints out numbers and lyrics
 for number in range(99,0,-1):
-    bottles.append(number)
     
-# for each number, we print out that number followed by the lyrics
-for num in bottles:
-    # if number is 1 or 2, the lyrics changes
-    # if not then we just print out the "standard"
-    if num != 1:
-        print(num, "bottles of beer on the wall,", num, "bottles of beer.")
+    # If the number is 1 or 2, the lyric changes
+    # Else, we print out the "standard" lyric"
+    if number != 1:
+        print(number, "bottles of beer on the wall,", number, "bottles of beer.")
         
-    if num is 2:
-        print("Take one down and pass it around,", (num-1), "bottle of beer on the wall.")
-    elif num is 1:
-        print(num, "bottle of beer on the wall,", num, "bottle of beer.")
+    if number is 2:
+        print("Take one down and pass it around,", (number-1), "bottle of beer on the wall.")
+        
+    elif number is 1:
+        print(number, "bottle of beer on the wall,", number, "bottle of beer.")
         print("Take one down and pass it around, no more bottles of beer on the wall")
+        
     else:
-        print("Take one down and pass it around,", (num-1), "bottles of beer on the wall.")
+        print("Take one down and pass it around,", (number-1), "bottles of beer on the wall.")
     print(" ")
     time.sleep(8)
     
+######################
+## FINISHING LYRICS ##
+######################
+
 print("No more bottles of beer on the wall, no more bottles of beer.")
 print("Go to the store and buy some more, 99 bottles of beer on the wall.")
+print(" ")
